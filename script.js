288ee0c3392eea9884b4c8ff071bb44203dcb3b0
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2024-02-11 21:35:01 -0300

    Filtro select funcionando

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
+const namesCollegesSelect = document.getElementsByClassName("name-college-select");
+const namesCollegesCards = document.getElementsByTagName("h2");
+const subtextCollegesCards = document.getElementsByTagName("h3");
+const buttonSearch = document.getElementById("button-search");
+
 function getColleges() {
   const url = `http://localhost:3000/colleges`;
+
   fetch(url)
     .then((response) => response.json())
-    .then((result) => displayResult(result));
+    .then((result) => {
+      displaySelect(result, namesCollegesSelect);
+      displayNamesColleges(result, namesCollegesCards);
+      displaySubtexto(result, subtextCollegesCards);
+    })
+    .catch((error) => {
+      console.log('Erro:', error); //Remover na produção
+    })
 }
 
-function displayResult(result) {
-  const namesColleges = document.getElementsByClassName("name-college");
-
-  for(i = 0; i < namesColleges.length; i++) {
-     namesColleges[i].value = result[i].name;
+function displaySelect(result, namesCollegesSelect) {
+  for(i = 0; i < namesCollegesSelect.length; i++) {
+    namesCollegesSelect[i].value = result[i].name;
   }
 }
+
+function displayNamesColleges(result, namesCollegesCards) {
+  for(i = 0; i < namesCollegesCards.length; i++) {
+    namesCollegesCards[i].innerHTML = result[i].name;
+  }
+}
+
+function displaySubtexto(result, subtextCollegesCards) {
+  for(i = 0; i < subtextCollegesCards.length; i++) {
+    subtextCollegesCards[i].innerHTML = result[i].city + " | " + result[i].state;
+  }
+}
+
+buttonSearch.addEventListener("click", function() {
+  const inputNameCollege = document.getElementById("input-name-college");
+  const cardsCollege = document.getElementsByClassName("card");
+
+  for(i = 0; i < cardsCollege.length; i++) {
+    if (namesCollegesCards[i].innerText.toLowerCase() === inputNameCollege.value.toLowerCase()) {
+      cardsCollege[i].classList.remove("hidden");
+      cardsCollege[i].classList.add("visible");
+    } else {
+      cardsCollege[i].classList.add("hidden");
+      cardsCollege[i].classList.remove("visible");
+    }
+  };
+});
 
 window.onload = getColleges();
